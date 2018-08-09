641fb210fe99753247a7300083af196172a11230
masamasa_uu <akatsukiryu848@gmail.com>
2018-08-09 11:14:37 +0900

    top cording

---
+++ js/script.js
@@ -0,0 +1,11 @@
+jQuery(function($) {
+    $('#Quietflow').quietflow({
+        theme: 'bouncingBalls',
+        specificColors : [
+            'rgba(255, 10, 50, 0.5)',
+            'rgba(10, 255, 50, 0.5)',
+            'rgba(10, 50, 255, 0.5)',
+            'rgba(0, 0, 0, 0.5)'
+        ]
+    })
+});
